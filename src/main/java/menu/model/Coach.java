c1660603029e1f472e569655b74f373f2d99aca5
jungyunmok <jungyunmok@gmail.com>
2023-12-14 21:02:46 +0900

    feat: 뽑은 숫자에 맞는 카테고리 반환 기능

--- src/main/java/menu/model/Coach.java
+++ src/main/java/menu/model/Coach.java
@@ -1,4 +1,32 @@
 package menu.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Coach {
+    private Map<String, List<String>> coachInfo = new HashMap<>();
+
+    // 인원 수 이름 검사 후 저장
+    public String[] validateCoach(String names) {
+        String[] coachNames = names.split(",");
+        if(coachNames.length < 2 || coachNames.length > 5) {
+            System.out.println("[ERROR] 식사는 최소 2명, 최대 5명 까지 가능합니다.");
+            throw new IllegalArgumentException();
+        }
+        for(String name : coachNames) {
+            if(name.length() < 2 || name.length() > 4) {
+                System.out.println("[ERROR] 코치 이름은 2~4글자여야 합니다.");
+                throw new IllegalArgumentException();
+            }
+        }
+        return coachNames;
+    }
+
+    // 코치별 먹지 못하는 음식 저장
+    public Map<String, List<String>> dislikeFood(String name, List<String> dislike) {
+        coachInfo.put(name, dislike);
+        return coachInfo;
+    }
 }
--- src/main/java/menu/model/Menu.java
+++ src/main/java/menu/model/Menu.java
@@ -1,4 +1,32 @@
 package menu.model;
 
+import java.util.*;
+
 public class Menu {
+    private List<String> japan = new ArrayList<>(Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"));
+    private List<String> korea = new ArrayList<>(Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"));
+    private List<String> china = new ArrayList<>(Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"));
+    private List<String> asian = new ArrayList<>(Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"));
+    private List<String> western = new ArrayList<>(Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+    private Map<Integer, Integer> history = new HashMap<>();
+
+    // 뽑은 숫자에 따라 카테고리 결정
+    public List<String> pickCategory(int number) {
+        if (number == 1) {
+            return japan;
+        } else if (number == 2) {
+            return korea;
+        } else if (number == 3) {
+            return china;
+        } else if (number == 4) {
+            return asian;
+        }
+        return western;
+    }
+
+    // 2회를 초과하는 추천 카테고리인지 검증
+
+    // 존재하는 메뉴인지 검증
+
+    // 중복되는 메뉴인지 검증
 }
