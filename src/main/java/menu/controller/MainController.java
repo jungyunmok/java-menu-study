fb015507d6946a79ff5792f13e2a5cce4d1b4d78
jungyunmok <jungyunmok@gmail.com>
2023-12-14 22:22:55 +0900

    feat: 추천 카테고리, 메뉴 산출 컨트롤러

--- src/main/java/menu/controller/MainController.java
+++ src/main/java/menu/controller/MainController.java
@@ -47,4 +47,28 @@ public class MainController {
         return coachInfo;
     }
 
+    // 카테고리 선택하고 그에 맞는 메뉴 반환하기
+    public List<String> pickCategory() {
+        int category = pickRandom.selectCategory();
+        List<String> menuList = null;
+        try {
+            menuList = menu.pickCategory(category);
+        } catch (IllegalArgumentException e) {
+            pickCategory();
+        }
+        return menuList;
+    }
+
+    // 추천 메뉴 반환하기
+    public void pickMenu(List<String> menuList, List<String> dislikeMenu) {
+        List<String> menuHistory = null;
+        do {
+            String pickedMenu = pickRandom.selectMenu(menuList, menuHistory, dislikeMenu);
+            menuHistory = menu.saveMenu(pickedMenu);
+        } while (menuHistory.size() == 5);
+    }
+
+
+    // 요일 선택해서 카테고리, 추천메뉴 출력하기
+
 }
