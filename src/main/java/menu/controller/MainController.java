00afa2c0600e3587c76b8787d5ffe10cad018658
jungyunmok <jungyunmok@gmail.com>
2023-12-14 22:04:25 +0900

    feat: 코치 이름 입력 컨트롤러

--- src/main/java/menu/controller/MainController.java
+++ src/main/java/menu/controller/MainController.java
@@ -3,6 +3,8 @@ package menu.controller;
 import menu.model.Coach;
 import menu.view.InputView;
 
+import java.util.List;
+
 public class MainController {
     InputView inputView;
     Coach coach;
@@ -10,6 +12,18 @@ public class MainController {
     // 점심 메뉴 추천 시작
     public void startMenu() {
         
+    }
+
+    // 코치 이름 입력
+    public List<String> setName() {
+        String names = inputView.readName();
+        List<String> coachNames = null;
+        try {
+            coachNames = coach.validateCoach(names);
+        } catch (IllegalArgumentException e) {
+            setName();
+        }
+        return coachNames;
     }
     
     // 코치별 못먹는 음식 입력
