2de6b5902be23c512498dfbfe93ec4af3527a92f
jungyunmok <jungyunmok@gmail.com>
2023-12-15 19:15:14 +0900

    feat: 못 먹는 메뉴 입력

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -5,7 +5,14 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     // 코치 이름 입력
     public String readName() {
-        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        System.out.println("\n코치의 이름을 입력해 주세요. (, 로 구분)");
+        String input = Console.readLine();
+        return input;
+    }
+
+    // 못 먹는 메뉴 입력
+    public String readName(String name) {
+        System.out.println("\n" + name + "(이)가 못 먹는 메뉴를 입력해 주세요.");
         String input = Console.readLine();
         return input;
     }
