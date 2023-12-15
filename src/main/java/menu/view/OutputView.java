1cbe42136c2093ded6e2e9efc0ec7f793193e326
jungyunmok <jungyunmok@gmail.com>
2023-12-15 19:16:48 +0900

    feat: 시작, 끝 문구 출력

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,7 +1,15 @@
 package menu.view;
 
 public class OutputView {
-    public void print() {
-        System.out.println("");
+    // 서비스 시작 문구
+    public void printStart() {
+        System.out.println("점심 메뉴 추천을 시작합니다.");
+    }
+
+    // 메뉴 추천 결과 출력
+
+    // 서비스 종료 문구
+    public void printEnd() {
+        System.out.println("\n추천을 완료했습니다.");
     }
 }
