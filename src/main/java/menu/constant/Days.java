e91cd924acebc4ae0a3f1416a3e99a3d9bf306ba
jungyunmok <jungyunmok@gmail.com>
2023-12-15 19:19:46 +0900

    feat: 요일 구분 출력

--- src/main/java/menu/constant/Days.java
+++ src/main/java/menu/constant/Days.java
@@ -2,5 +2,5 @@ package menu.constant;
 
 public class Days {
     // 총 요일
-    public static final String[] DAYS = {"월", "화", "수", "목", "금"};
+    public static final String[] DAYS = {"월요일", "화요일", "수요일", "목요일", "금요일"};
 }
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,12 +1,25 @@
 package menu.view;
 
+import menu.constant.Days;
+
 public class OutputView {
     // 서비스 시작 문구
     public void printStart() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
     }
 
-    // 메뉴 추천 결과 출력
+    // 메뉴 추천 결과 - 요일 출력
+    public void printDays() {
+        System.out.print("\n[ 구분 ");
+        for(String day : Days.DAYS) {
+            System.out.print("| "+day);
+        }
+        System.out.print(" ]");
+    }
+
+    // 메뉴 추천 결과 - 카테고리 출력
+
+    // 메뉴 추천 결과 - 코치별 추천 메뉴 출력
 
     // 서비스 종료 문구
     public void printEnd() {
